7e53e005ac3d14b1335c0aa1ad21a5637de255ab
Mint Rosetta <mint.rosetta2001@gmail.com>
2024-06-03 15:40:17 +0700

    create user by manage user page

--- ShopmeProject/ShopmeWebParents/ShopmeAdmin/src/main/java/com/shopme/admin/user/UserController.java
+++ ShopmeProject/ShopmeWebParents/ShopmeAdmin/src/main/java/com/shopme/admin/user/UserController.java
@@ -5,8 +5,12 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import com.shopme.commons.entity.Role;
 import com.shopme.commons.entity.User;
 
 @Controller
@@ -20,10 +24,32 @@ public class UserController {
 	
 	@GetMapping("")
 	public String viewAllUser(Model model) {
-		List<User> users = this.userService.listAll();
-		
+		List<User> users = this.userService.getUsers();
+
 		model.addAttribute("users", users);
 		
 		return "users/index";
+	}
+	
+	@GetMapping("create")
+	public String viewCreateUser(Model model) {
+		User user = new User();
+		user.setEnabled(true);
+		List<Role> roles = this.userService.getRoles();
+		
+		model.addAttribute("user", user);
+		model.addAttribute("roles", roles);
+		
+		return "users/create";
+	}
+	
+	@PostMapping("create")
+	public String createUser(@ModelAttribute("user") User user, RedirectAttributes redirectAttributes) {
+		System.out.println(user.toString());
+		this.userService.save(user);
+		
+		// use for past data to redirect url
+		redirectAttributes.addFlashAttribute("message", "The user has been saved successfully.");
+		return "redirect:/users";
 	}
 }
--- ShopmeProject/ShopmeWebParents/ShopmeAdmin/src/main/java/com/shopme/admin/user/UserService.java
+++ ShopmeProject/ShopmeWebParents/ShopmeAdmin/src/main/java/com/shopme/admin/user/UserService.java
@@ -4,17 +4,28 @@ import java.util.List;
 
 import org.springframework.stereotype.Service;
 
+import com.shopme.commons.entity.Role;
 import com.shopme.commons.entity.User;
 
 @Service
 public class UserService {
 	private UserRepository userRepository;
+	private RoleRepository roleRepository;
 	
-	public UserService(UserRepository userRepository) {
+	public UserService(UserRepository userRepository, RoleRepository roleRepository) {
 		this.userRepository = userRepository;
+		this.roleRepository = roleRepository;
 	}
 	
-	public List<User> listAll() {
+	public List<User> getUsers() {
 		return (List<User>) this.userRepository.findAll();
+	}
+	
+	public List<Role> getRoles() {
+		return (List<Role>) this.roleRepository.findAll();
+	}
+
+	public void save(User user) {
+		this.userRepository.save(user);
 	}
 }
